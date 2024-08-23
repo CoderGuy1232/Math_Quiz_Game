3912411f426432de84dc071f146041c19b1f0f52
CoderGuy1232 <124039703+CoderGuy1232@users.noreply.github.com>
2024-08-23 18:04:56 +0400

    Update mathgamelib.py

--- mathgamelib.py
+++ mathgamelib.py
@@ -1,1 +1,18 @@
+class TreeA:
+    def Q1():
+        while True:
+            answer = int(input("Question 1: What is 5 + 10? "))
+            if answer == 15:
+                print("correct")
+                break
+            else:
+                print("try again")
+    def Q2():
+        while True:
+            answer = int(input("Question 2: What is 25 + 10? "))
+            if answer == 35:
+                print("correct")
+                break
+            else:
+                print("try again")
 
